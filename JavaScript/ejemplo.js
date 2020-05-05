f588616679689ec89e994a3e5c4159ce88cc33a3
enriquedbz <mlopezq@davivienda.com>
2020-05-05 08:18:14 -0500

    Vectores

---
+++ JavaScript/ejemplo.js
@@ -0,0 +1,126 @@
+
+
+
+
+
+
+
+var miVariableEntera;
+miVariableEntera = 43232;
+
+var miVariableCadena = "Cristian";
+miVariableCadena = 1212;
+
+var miFloat = 10.2
+var miVaribleNumero = '2'
+
+
+var miArreglo = [1,2,3, 'a', 'b'];
+console.log(miArreglo)
+
+
+console.log("Hola"*2);
+
+
+var a =2 ;
+
+var b =2
+var x = 4;
+var y = 5;
+
+if(a==b){
+    console.log("Son diferentess");
+}else { 
+    console.log("b es mayor que y");
+}
+
+var miarrayEjemplo = ['a','b','c']
+var indice = 0;
+var a =[3,23]
+var x = a[1] == 23
+
+console.log(a[0]);
+
+for (let index = 0; index < miarrayEjemplo.length; index++) {
+   console.log(miarrayEjemplo[index])
+    
+}
+
+//********************************************************************************************** */
+
+
+ var numeros = [3,54,34,1,0,2,12,29,5];
+ var i;
+ var j; 
+ var tmp;
+
+
+ for(let i = 0; i < numeros.length ; i++)
+ {
+     for(let j = 0; j < numeros.length ; j++)
+     {
+         if (numeros[j] > numeros[j + 1])
+         {
+             let tmp = numeros[j+1];
+             numeros[j+1] = numeros[j];
+             numeros[j] = tmp;
+         }
+     }
+     
+ }
+ console.log(numeros);
+
+ var arr = [3,54,34,1,0,2,12,29,5];
+ function comparar ( a, b ){ return a - b; }
+ console.log(arr.sort( comparar ));  // [ 1, 5, 40, 200 ]
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ // for(let i = 0;i < numeros.length; i++)
+ //{
+   //  console.log(numeros[i]);
+ //}
+
+/*
+var miarrayEjemplo = ['a','b','c']
+var indice = 0;
+var a =[3,23]
+var x = a[1] == 23
+
+console.log(a[0]);
+
+for (let index = 0; index < miarrayEjemplo.length; index++) {
+   console.log(miarrayEjemplo[index])
+
+
+
+*/
+
+
+
+function elNombreDeLaFuncion(){
+ //Todo el contenido de mi funcion   
+
+ /*
+ lo que
+ quiero 
+ comentar
+ */
+
+}
+
+
+
+
+
